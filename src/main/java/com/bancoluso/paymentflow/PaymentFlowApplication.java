e7f63f90ea4f8d0213813b94632f8570d9cefe89
Iheb Raddaoui <ihebraddewy@gmail.com>
2026-08-08 10:40:36 +0100

    chore: init spring boot project

---
+++ src/main/java/com/bancoluso/paymentflow/PaymentFlowApplication.java
@@ -0,0 +1,13 @@
+package com.bancoluso.paymentflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentFlowApplication.class, args);
+    }
+
+}
